048f43ac5e1a19b01720d55a1e6b08d596caecd2
debendrabose12345 <debendrabose12345@gmail.com>
2024-10-04 09:28:04 +0000

    first commit

---
+++ Week1/Basic.js
@@ -0,0 +1,22 @@
+let name = "debajyoti";
+let age = 22;
+
+let user1 = {
+    name: "debajyoti",
+    age: 22
+}
+
+console.log(user1);
+console.log(user1.name);
+console.log(user1.age);
+
+let users = ["aman", "bishal", "debajyoti", "santosh"];
+console.log(users);
+console.log(users[0]);
+console.log(users[1]);
+console.log(users[2]);
+console.log(users[3]);
+
+for(let i = 0; i < users.length; i++){
+    console.log(users[i]);
+}
